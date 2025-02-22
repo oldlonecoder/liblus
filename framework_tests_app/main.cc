17b94dcdaf29c99fb8770ac7df028cab1812ec84
oldlonecoder <lussier.serge@gmail.com>
2025-02-22 14:32:18 -0500

    Progression

--- framework_tests_app/main.cc
+++ framework_tests_app/main.cc
@@ -15,7 +15,9 @@ public:
     rem::cc run() override;
     rem::cc setup() override;
     rem::cc setup_ui() override;
-
+//    rem::cc terminate(rem::type reason_type) override;
+protected:
+    rem::cc setup_journal() override;
 };
 
 lus_app::lus_app(const std::string& app_id, lus::string::view_list cargs): lus::ui::application(app_id, cargs){}
@@ -23,7 +25,9 @@ lus_app::~lus_app(){}
 
 rem::cc lus_app::run()
 {
-    return lus::ui::application::run();
+    lus::ui::application::run();
+    terminate(rem::type::test);
+    return rem::cc::terminate;
 }
 
 rem::cc lus_app::setup()
@@ -36,6 +40,20 @@ rem::cc lus_app::setup_ui()
     return ui::application::setup_ui();
 }
 
+
+// rem::cc lus_app::terminate(rem::type reason_type)
+// {
+//     return ui::application::app().terminate(reason_type);
+// }
+
+rem::cc lus_app::setup_journal()
+{
+    journal::init("lus.framework.API.tests.application");
+    journal::status() << rem::cc::ready << journal::eol;
+    return rem::cc::ready;
+}
+
+
 }
 
 
@@ -43,5 +61,6 @@ rem::cc lus_app::setup_ui()
 auto main(int argc, char** argv, char** env) -> int
 {
     std::cout << "Hello, World\n";
-    return 0;
+    lus::lus_app app{"lus_app tests", lus::string::string_view_list(argc, argv,1)};
+    return static_cast<int>(app.run());
 }
